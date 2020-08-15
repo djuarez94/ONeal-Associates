44ecf17a65b6a47375579441445ef6f64fba75b3
David <davidjuarez1411@gmail.com>
2020-08-14 17:20:33 -0700

    updated css for mobile and added animate on scroll effects to elements for all devices

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
+//Initiate animate on scroll effects
+AOS.init();
+
 //Solution Content Reveal : Home
 var serviceLink = document.querySelectorAll("#services a.serviceLink");
 
@@ -327,6 +330,44 @@ for (var i = 0 ; i < teamMembers.length; i++) {
 
 }
 
-//Solution Content Reveal : Home
+//Copy email to clipboard
+
+  // Add class to mailto link
+	// Needed to separate the disabling of the default action AND copy email to clipboard
+	$('a[href^=mailto]').addClass('mailto-link');
+
+	var mailto = $('.mailto-link');
+	var messageCopy = 'Click to copy email address';
+	var messageSuccess = 'Email address copied to clipboard';
+
+	mailto.append('<span class="mailto-message"></span>');
+	$('.mailto-message').append(messageCopy);
+
+	// Disable default action (opening your email client. yuk.)
+	$('a[href^=mailto]').click(function() {
+		return false;
+	})
+
+	// On click, get href and remove 'mailto:'
+	// Store email address in a variable.
+	mailto.click(function() {
+		var href = $(this).attr('href');
+		var email = href.replace('mailto:', '');
+		copyToClipboard(email);
+		$('.mailto-message').empty().append(messageSuccess);
+		setTimeout(function() {
+			$('.mailto-message').empty().append(messageCopy);}, 2000);
+	});
+
+	// Grabbed this from Stack Overflow.
+  // Copies the email variable to clipboard
+  function copyToClipboard(text) {
+      var dummy = document.createElement("input");
+      document.body.appendChild(dummy);
+      dummy.setAttribute('value', text);
+      dummy.select();
+      document.execCommand('copy');
+      document.body.removeChild(dummy);
+  }
 
 });
